1defde5dbed8f09bcf917be7ac0eafa74063def3
Victor <53414263+vuduland@users.noreply.github.com>
2020-06-11 17:14:50 -0500

    changed min password length

--- src/routers/user.js
+++ src/routers/user.js
@@ -11,7 +11,6 @@ router.post('/users', async (req, res) => {
 		await user.save();
 		const token = await user.generateAuthToken();
 		res.status(201).send({ user, token });
-		console.log(token);
 	} catch (error) {
 		res.status(400).send(error);
 	}
