874201980f16e2bf747c6b756ef1fbe915dc1269
Peter Smith <petergbsmith@gmail.com>
2020-06-14 17:10:52 -0500

    register-route

--- public/js/signup.js
+++ public/js/signup.js
@@ -6,28 +6,28 @@ $(document).ready(function() {
   var passwordInput = $("input#new-password-input");
 
   // When the signup button is clicked, we validate the name, email, and password are not blank
-  signUpForm.on("submit", function(event) {
-    event.preventDefault();
-    var userData = {
-      name: nameInput.val().trim(),
-      email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
-    };
+  // signUpForm.on("submit", function(event) {
+  //   event.preventDefault();
+  //   var userData = {
+  //     name: nameInput.val().trim(),
+  //     email: emailInput.val().trim(),
+  //     password: passwordInput.val().trim()
+  //   };
 
-    if (!userData.name || !userData.email || !userData.password) {
-      return;
-    }
-    // If we have a name, email, and password, run the signUpUser function
-    signUpUser(userData.name, userData.email, userData.password);
-    nameInput.val("");
-    emailInput.val("");
-    passwordInput.val("");
-  });
+  //   if (!userData.name || !userData.email || !userData.password) {
+  //     return;
+  //   }
+  //   // If we have a name, email, and password, run the signUpUser function
+  //   signUpUser(userData.name, userData.email, userData.password);
+  //   nameInput.val("");
+  //   emailInput.val("");
+  //   passwordInput.val("");
+  // });
 
   // Does a post to the signup route. If successful, we are redirected to the intake form page
   // Otherwise we log any errors
   function signUpUser(name, email, password) {
-    $.post("/api/signup", {
+    $.post("/users/register", {
       name: name,
       email: email,
       password: password
